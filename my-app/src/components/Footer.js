3880bb1b9f7824bcf65cdd8db853455574294086
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-10 23:16:02 +0200

    adding images

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,52 +1,59 @@
 import React from 'react'
 import '../styles/Footer.css'
+import reactLogo from "../images/maps.png";
 
 function Footer() {
     return (
         <div>
-        
-        <footer class="footer-distributed">
 
-			<div class="footer-left">
+            <footer class="footer-distributed">
 
-				<h3>SGRUP<span>.CB</span></h3>
+                <div class="footer-left">
 
-				<p class="footer-company-name">SGRUP.CB © 2016</p>
-			</div>
+                    <h3>SGRUP<span>.CB</span></h3>
 
-			<div class="footer-center">
+                    <p class="footer-company-name">SGRUP.CB © 2016</p>
+                    <img  src={reactLogo}/>
+                </div>
 
-				<div>
-					<i class="fa fa-map-marker"></i>
-					<p><span>Carrer Santa Bárbara, 46712</span> Piles, València, Spain</p>
-				</div>
+                <div class="footer-right">
 
-				<div>
-					<i class="fa fa-phone"></i>
-					<p>+34 605 50 37 49</p>
-                    <p>+34 642 89 10 00</p>
-				</div>
+                    <p class="footer-company-about">
+                        <span>Transporte, compra y venta de cítricos y otras frutas.</span>
+                        En SGRUP.CB 2016 contamos con una amplia trayectoria profesional en el sector agrario en concreto cítricos para la industria y el mercado. Disponemos de una amplia variedad de cítricos que llegan de la tierra directo en su mesa.
+                        Disponemos de transporte propio para ofrecerles un mejor servicio.
+                    </p>
+                    <a target="_blank" href="https://icons8.com/icon/J7cwU7OY5OnR/orange">Orange</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+                </div>
+                
 
-				<div>
-					<i class="fa fa-envelope"></i>
-					<p><a href="sgrup.bg@gmail.com">sgrup.bg@gmail.com</a></p>
-				</div>
+                <div class="footer-center">
 
-			</div>
+                    <div>
+                        <i class="fa fa-map-marker"></i>
+                        <p><span>Carrer Santa Bárbara, 46712</span> Piles, València, Spain</p>
+                    </div>
 
-			<div class="footer-right">
+                    <div>
+                        <i class="fa fa-phone">
+                            
+                            <p>+34 605 50 37 49</p>
+                            <p> +34 642 89 10 00</p>
+                        </i>
+                    </div>
 
-				<p class="footer-company-about">
-					<span>Transporte, compra y venta de cítricos y otras frutas.</span>
-					En SGRUP.CB © 2016 contamos con una amplia trayectoria profesional en el sector de los cítricos con destino a industria. Realizamos la retirada de productos por rutas nacionales, tanto en explotaciones agrícolas como en almacenes.
-				</p>
+                    <div>
+                        <i class="fa fa-envelope"></i>
+                        <p><a href="sgrup.bg@gmail.com">sgrup.bg@gmail.com</a></p>
+                    </div>
 
-				
-
-                <a target="_blank" href="https://icons8.com/icon/J7cwU7OY5OnR/orange">Orange</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-			</div>
-		</footer>
-    </div>
+                </div>
+                
+                <div class="copyright">
+                SGRUP.CB © 2016 | Todos los derechos reservados  |
+                </div>
+            </footer>
+        </div>
     )
 }
 
