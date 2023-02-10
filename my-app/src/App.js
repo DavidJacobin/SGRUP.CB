9b149d614be345aa961c78a4eabed4235d4a0c51
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-10 21:21:09 +0200

    adding logo header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Home} from './pages/Home'
+import Navbar from './components/Navbar';
+import Title from './components/Header';
+
+
 function App() {
   return (
     <div className="App">
       <Router>
+        <Title/>
+        <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}>
 
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Header.css'
+import reactLogo from "../images/MS.png";
+function Title() {
+    return (
+        <><div className="header" />
+            <img src={reactLogo}/>
+            
+        <div className="header-right" /></>
+
+    )
+}
+
+export default Title
