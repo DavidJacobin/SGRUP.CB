f183f0b0f20bc19f58a746c0daa45703825be2c1
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-02-11 13:33:28 +0200

    adding images and text over pics

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -5,30 +5,49 @@ import Footer from '../components/Footer'
 import photo1 from "../images/home/1.jpg";
 import photo2 from "../images/home/2.jpg";
 import photo3 from "../images/home/3.jpg";
-import photo4 from "../images/home/4.jpg";
 import photo5 from "../images/home/5.jpg";
 import photo6 from "../images/home/6.jpg";
+import photo7 from "../images/home/7.jpg";
 
 
 
 export const Home = () => {
   return (
     <div>
-     <body>
+     <body className='gird'>
       <section className='basic-grid'>
-        <div className='card card-tall card-wide'><img src={photo1}/></div>
-        <div className='card card-tall card-wide'><img src={photo2}/></div>
-        <div className='card card-tall card-wide'><img src={photo3}/></div>
-        <div className='card card-tall card-wide'><img src={photo4}/></div>
-        <div className='card card-tall card-wide'><img src={photo5}/></div>
-        <div className='card card-tall card-wide'><img src={photo6}/></div>
-        
-        
-        
-        
-
+        <div className='card card-tall card-wide'>
+          
+          <img src={photo7}/>
+        </div>
+        <div className='card card-tall card-wide'>
+      
+          <img src={photo2}/>
+          </div>
+        <div className='card card-tall card-wide'>
+          <img src={photo3}/>
+        </div>
+        <div className='card card-tall card-wide'>
+          <h1>Clementinas</h1>
+          <img src={photo1}/>
+        </div>
+        <div className='card card-tall card-wide'>
+          <h1>Naranjas</h1>
+          <img src={photo6}/>
+        </div>
+        <div className='card card-tall card-wide'>
+          <h1>Limones</h1>
+          <img src={photo5}/>
+        </div>
       </section>
      </body>
+     <body>
+        <p>En SGRUP.CB 2016 contamos con una amplia trayectoria profesional en el sector agrario en concreto cítricos para la industria y el mercado. Disponemos de una amplia variedad de cítricos que llegan de la tierra directo en su mesa.
+                        Disponemos de transporte propio para ofrecerles un mejor servicio.
+        </p>
+     </body>
+      
+
     </div>
   )
 }
